b087d3c32e1923be6c0648bc6471206b3b939adf
limin9yuan <limin9yuan@gmail.com>
2018-07-11 16:25:02 +0800

    not close refresh parent window done

--- Project1/source/src/main/resources/static/js/appjs/contract/contract/receivable.js
+++ Project1/source/src/main/resources/static/js/appjs/contract/contract/receivable.js
@@ -32,7 +32,7 @@ function loadReceivable() {
 								//说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
 								limit: params.limit,
 								offset:params.offset,
-								contractId:$('#contractId').val()
+								contractId:$('#contractIds').val()
 					           // name:$('#searchName').val(),
 					           // username:$('#searchName').val()
 							};
